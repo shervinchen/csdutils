13d73cd1fb6115b2e1f4f2491ea838a767dfc0d2
csdoker <csd758371536@qq.com>
2018-03-04 23:50:04 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
-const babel = require("gulp-babel");
+// const babel = require("gulp-babel");
 
 // gulp.task('babel', function() {
 // 	gulp.src('src/csdutils.js')
