77d09301c97e727f3075ff55fe8da0dc688d7ff2
csdoker <csd758371536@qq.com>
2018-04-22 19:42:47 +0800

    modify

--- src/csdutils.js
+++ src/csdutils.js
@@ -3880,41 +3880,72 @@
         };
       }
     },
-    // 跨浏览器addEvent
-    addEvent: function(node, type, handler) {
-      if (!node) return false;
-      if (node.addEventListener) {
-        node.addEventListener(type, handler, false);
-        return true;
-      } else if (node.attachEvent) {
-        node['e' + type + handler] = handler;
-        node[type + handler] = function() {
-          node['e' + type + handler](win.event);
-        };
-        node.attachEvent('on' + type, node[type + handler]);
-        return true;
-      }
-      return false;
-    },
-    // 跨浏览器removeEvent
-    removeEvent: function(node, type, handler) {
-      if (!node) return false;
-      if (node.removeEventListener) {
-        node.removeEventListener(type, handler, false);
-        return true;
-      } else if (node.detachEvent) {
-        node.detachEvent('on' + type, node[type + handler]);
-        node[type + handler] = null;
-        return true;
-      }
-      return false;
-    },
+    // 页面加载完成后
+		readyEvent : function(fn) {
+			if (fn==null) {
+				fn=document;
+			}
+			var oldonload = window.onload;
+			if (typeof window.onload != 'function') {
+				window.onload = fn;
+			} else {
+				window.onload = function() {
+					oldonload();
+					fn();
+				};
+			}
+		},
+    // 视能力分别使用dom0||dom2||IE方式 来绑定事件
+		// 参数： 操作的元素,事件名称 ,事件处理程序
+		addEvent : function(element, type, handler) {
+			if (element.addEventListener) {
+				element.addEventListener(type, handler, false);
+			} else if (element.attachEvent) {
+				element.attachEvent('on' + type, function() {
+					handler.call(element);
+				});
+			} else {
+				element['on' + type] = handler;
+			}
+		},
+    // 移除事件
+		removeEvent : function(element, type, handler) {
+			if (element.removeEventListener) {
+				element.removeEventListener(type, handler, false);
+			} else if (element.datachEvent) {
+				element.detachEvent('on' + type, handler);
+			} else {
+				element['on' + type] = null;
+			}
+		},
+    // 阻止事件流
+		stopEvent : function(ev) {
+			this.stopPropagation(ev);
+			this.preventDefault(ev);
+		},
     getEvent: function(event) {
       return event ? event : window.event;
     },
+    // 获取event对象的引用，取到事件的所有信息，确保随时能使用event；
+		// getEvent : function(e) {
+		// 	var ev = e || window.event;
+		// 	if (!ev) {
+		// 		var c = this.getEvent.caller;
+		// 		while (c) {
+		// 			ev = c.arguments[0];
+		// 			if (ev && Event == ev.constructor) {
+		// 				break;
+		// 			}
+		// 			c = c.caller;
+		// 		}
+		// 	}
+		// 	return ev;
+		// },
+    // 获取事件目标
     getTarget: function(event) {
       return event.target || event.srcElement;
     },
+    // 阻止事件 (主要是事件冒泡，因为IE不支持事件捕获)
     stopPropagation: function(event) {
       if (event.stopPropagation) {
         event.stopPropagation();
@@ -3922,6 +3953,7 @@
         event.cancelBubble = true;
       }
     },
+    // 取消事件的默认行为
     preventDefault: function(event) {
       if (event.preventDefault) {
         event.preventDefault();
