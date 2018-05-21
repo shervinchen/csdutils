cbb95231c52e22e54f1e4650e99fd3f94ced8f02
csdoker <csd758371536@qq.com>
2018-05-21 22:02:11 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
-// const babel = require("gulp-babel");
+const babel = require("gulp-babel");
 
 // gulp.task('babel', function() {
 // 	gulp.src('src/csdutils.js')
@@ -14,6 +14,9 @@ const rename = require("gulp-rename");
 
 gulp.task('mini', function() {
   gulp.src('src/csdutils.js')
+  .pipe(babel({
+    presets: ['es2015']
+  }))
   .pipe(uglify())    //uglify
   .pipe(rename("csdutils.min.js"))
   .pipe(gulp.dest('dist/'))
--- src/csdutils.js
+++ src/csdutils.js
@@ -621,7 +621,7 @@
      * @param {String} ARepText 替换的字符串
      */
     replaceAll: function(str, AFindText, ARepText) {
-      raRegExp = new RegExp(AFindText, "g");
+      var raRegExp = new RegExp(AFindText, "g");
       return str.replace(raRegExp, ARepText);
     },
     /**
@@ -1150,6 +1150,31 @@
   };
   // 数组工具
   csdutils.arrayUtils = {
+    //从一个给定的数组arr中,随机返回num个不重复项
+    getArrayItems: function(arr, num) {
+        //新建一个数组,将传入的数组复制过来,用于运算,而不要直接操作传入的数组;
+        var temp_array = new Array();
+        for (var index in arr) {
+            temp_array.push(arr[index]);
+        }
+        //取出的数值项,保存在此数组
+        var return_array = new Array();
+        for (var i = 0; i<num; i++) {
+            //判断如果数组还有可以取出的元素,以防下标越界
+            if (temp_array.length>0) {
+                //在数组中产生一个随机索引
+                var arrIndex = Math.floor(Math.random()*temp_array.length);
+                //将此随机索引的对应的数组元素值复制出来
+                return_array[i] = temp_array[arrIndex];
+                //然后删掉此索引的数组元素,这时候temp_array变为新的数组
+                temp_array.splice(arrIndex, 1);
+            } else {
+                //数组中数据项取完后,退出循环,比如数组本来只有10项,但要求取出20项.
+                break;
+            }
+        }
+        return return_array;
+    },
     // 检测是否是数组
     judgeArr: function(arr) {
       if (Array.isArray(arr)) {
@@ -3143,7 +3168,7 @@
     insertAtCursor: function(dom, val) {
       if (document.selection) {
         dom.focus();
-        sel = document.selection.createRange();
+        var sel = document.selection.createRange();
         sel.text = val;
         sel.select();
       } else if (dom.selectionStart || dom.selectionStart == '0') {
@@ -3930,11 +3955,11 @@
       try {
         t2 = arg2.toString().split(".")[1].length
       } catch (e) {}
-      with(Math) {
+      // with(Math) {
         r1 = Number(arg1.toString().replace(".", ""));
         r2 = Number(arg2.toString().replace(".", ""));
-        return (r1 / r2) * pow(10, t2 - t1);
-      }
+        return (r1 / r2) * Math.pow(10, t2 - t1);
+      // }
     },
     // 判断网页元素是否具有某种属性和样式 matchesSelector
     // Usage
